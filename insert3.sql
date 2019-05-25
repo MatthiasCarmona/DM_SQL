6f392973533c795b042f9f9aba3bba22876956f1
Nico3652 <50270977+Nico3652@users.noreply.github.com>
2019-05-25 18:09:53 +0200

    Ajout insertion 3

---
+++ insert3.sql
@@ -0,0 +1,11 @@
+INSERT INTO membre (Numero,Nom,Prenom,Adresse,Telephone,Adhesion,Duree)
+VALUES (1,"ALBERT","Anne","13 rue des Alpes","0601020304",SYSDATE()-60,1),
+(2,"BERNAUD","Barnabé","6 rue des bécasses","0602030105",SYSDATE()-10,3),
+(3,"CUVARD","CAMILLE","52 rue des cerisiers","0602010509",SYSDATE()-100,6),
+(4,"DUPOND","DANIEL","11 rue des daims","0610236515",SYSDATE()-250,12),
+(5,"EVROUX","Eglantine","34 rue des elfes","0658963125",SYSDATE()-150,6),
+(6,"FREGEON","Fernand","11 rue des Frans","0602036987",SYSDATE()-400,6),
+(7,"GORIT","Gaston","96 rue de la glacerie","0684235781",SYSDATE()-150,1),
+(8,"HEVARD","Hector","12 rue haute","0608546578",SYSDATE()-250,12),
+(9,"INGRAND ","Irène","54 rue de iris","0605020409",SYSDATE()-50,12),
+(10,"JUSTE","Julien","5 place de Jacobins","0603069876",SYSDATE()-100,6);
